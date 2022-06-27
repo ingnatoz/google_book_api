a32eedfa4428751fd96d46e16d03de81ffb15dc6
ingnatoz <54222351+ingnatoz@users.noreply.github.com>
2022-06-27 03:54:28 -0500

    ALLOWED_HOSTS ignore

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,10 +1,6 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
